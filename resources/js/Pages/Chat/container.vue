b8df62e656a1956e1dfa0c996c5d9d48e660527c
SaifN97 <saifnarpali@gmail.com>
2021-02-19 22:53:08 +0530

    Added chat frontend layout and model logic

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -2,15 +2,20 @@
 <app-layout>
     <template #header>
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Chat
+            <chat-room-selection
+                v-if="currentRoom.id"
+                :rooms="chatRooms"
+                :currentRoom="currentRoom"
+                v-on:roomChanged="setRoom( $event )"
+            />
         </h2>
     </template>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <message-container />
-                <input-message />
+                <message-container :messages="messages" />
+                <input-message :room="currentRoom" v-on:messageSent="getMessages()"/>
             </div>
         </div>
     </div>
@@ -21,12 +26,54 @@
 import AppLayout from '@/Layouts/AppLayout'
 import MessageContainer from './messageContainer.vue'
 import InputMessage from './inputMessage.vue'
+import ChatRoomSelection from './chatRoomSelection.vue'
 
 export default {
     components: {
         AppLayout,
         MessageContainer,
-        InputMessage
+        InputMessage,
+        ChatRoomSelection
     },
+
+    data: () => {
+        return{
+            chatRooms: [],
+            currentRoom: [],
+            messages: [],
+        }
+    },
+
+    methods:{
+        getRooms(){
+            axios.get('/chat/rooms')
+            .then(res => {
+                this.chatRooms = res.data;
+                this.setRoom(res.data[0]); 
+            })
+            .catch(err => {
+                console.error(err); 
+            })
+        },
+
+        setRoom(room){
+            this.currentRoom = room;
+            this.getMessages();
+        },
+
+        getMessages(){
+            axios.get('/chat/room/'+ this.currentRoom.id + '/messages')
+            .then(res => {
+                this.messages = res.data;
+            })
+            .catch(err => {
+                console.error(err); 
+            })
+        }
+    },
+
+    created(){
+        this.getRooms();
+    }
 }
 </script>
